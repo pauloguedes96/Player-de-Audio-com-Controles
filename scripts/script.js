f4954a3f9018a1f1ea87113a16814079c3b8ba32
Paulo Guedes <52800461+pauloguedes96@users.noreply.github.com>
2022-01-07 18:11:05 -0300

    atualizacao

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ let nome = document.getElementById("nomeArtista");
 player.addEventListener("timeupdate", progresso);
 
 //Array das Capas
-let capas = ["assets/image/cris-brown.jpg","assets/image/Erik Hecht.jpg","assets/image/Justin Bieber.jpg","assets/image/Yves Larock.jpg"]
+let capas = ["assets/image/cris-brown.jpg", "assets/image/Erik Hecht.jpg", "assets/image/Justin Bieber.jpg", "assets/image/Yves Larock.jpg"]
 
 //Array dos Nomes
 let nomeArtista = ["Chris Brown", "Erik Hecht", "Justin Bieber", "Yves Larock"]
@@ -38,22 +38,26 @@ let btnPause = document.getElementsByClassName("btnPlayer")[2];
 
 function back() {
 
-    if (i >= 1){
+    if (i >= 1) {
         i--;
         console.log(i);
         capa.src = capas[i];
         nome.innerHTML = `Artista: ${nomeArtista[i]}`;
         console.log(capa.src);
+
+        document.getElementById("fim").style.display = 'block';
     }
 
-    if( (i < musicas.length) && (i >= 0) ) {
+    if ((i < musicas.length) && (i >= 0)) {
         player.pause();
-        
+
         player.src = musicas[i].getAttribute("src");
         player.load();
-        player.play();     
-    }else
-    {
+        player.play();
+
+        document.getElementById("fim").style.display = 'block';
+        
+    } else {
         console.log("Acabou as Músicas");
     }
 }
@@ -68,17 +72,17 @@ var reproduzindo = "";
 
 function reproduzir() {
 
-    if ( reproduzindo == true) {
+    if (reproduzindo == true) {
         btnPause.src = "./assets/icons-player/play-circle.svg"
         player.pause();
         console.log("pausou");
 
         reproduzindo = false;
 
-    }else{
+    } else {
         player.play();
         //console.log(i);
-        
+
         btnPause.src = "./assets/icons-player/pause.svg"
         //let pause = false;
 
@@ -89,6 +93,8 @@ function reproduzir() {
         //progresso();
 
         volume();
+
+        document.getElementById("fim").style.display = 'block';
     }
 
 }
@@ -102,9 +108,11 @@ function parar() {
 
     //zera o pause
     reproduzindo = "";
+
+    document.getElementById("fim").style.display = 'none';
 }
 
-function pause(){
+function pause() {
 
     player.pause();
 }
@@ -117,35 +125,40 @@ function avancar() {
 
 function next() {
 
-    if (i < ((musicas.length) - 1)){
+    if (i < ((musicas.length) - 1)) {
         i++;
         console.log(i);
         capa.src = capas[i];
         console.log(capa.src);
         nome.innerHTML = `Artista: ${nomeArtista[i]}`;
         console.log(nome);
+        btnPause.src = "./assets/icons-player/pause.svg"
+
+        document.getElementById("fim").style.display = 'block';
     }
 
-    if(i < musicas.length){
+    if (i < musicas.length) {
         player.pause();
-        
+
         player.src = musicas[i].getAttribute("src");
         player.load();
-        player.play();     
+        player.play();
+        btnPause.src = "./assets/icons-player/pause.svg"
 
-    }else
-    {
+        document.getElementById("fim").style.display = 'block';
+
+    } else {
         console.log("Acabou as Músicas");
     }
 
 }
 
 function progresso() {
-    
+
     let progresso = document.getElementById("progresso");
 
     //Fazendo o progresso na barra
-    progresso.style.width = Math.floor((player.currentTime / player.duration) * 100) + '%'; 
+    progresso.style.width = Math.floor((player.currentTime / player.duration) * 100) + '%';
 
     let tempoDecorrido = document.getElementById("inicio");
 
@@ -156,11 +169,11 @@ function progresso() {
 
 }
 
-function segundosParaMinutos(segundos){
+function segundosParaMinutos(segundos) {
 
     let campoMinutos = Math.floor(segundos / 60);
     let campoSegundos = segundos % 60;
-    if (campoSegundos < 10){
+    if (campoSegundos < 10) {
         campoSegundos = '0' + campoSegundos;
     }
 
@@ -168,10 +181,53 @@ function segundosParaMinutos(segundos){
 }
 
 //função para definir volume
-function volume () {
+function volume() {
 
     let inputVol = document.getElementById("volume");
     player.volume = inputVol.value / 100;
-    
-    //console.log(inputVol.value);
+
+    /* console.log(inputVol.value); */
+
+    if (inputVol.value / 100 < 0.5) {
+
+        /* console.log("pegou volume"); */
+
+        document.getElementById('iconVol').src = "./assets/icons-player/volume_down_white_24dp.svg";
+    }
+    if (inputVol.value / 100 > 0.5) {
+
+        document.getElementById('iconVol').src = "./assets/icons-player/volume_up_white_24dp.svg";
+    }
+    if (inputVol.value / 100 == 0.0) {
+        document.getElementById('iconVol').src = "./assets/icons-player/volume_mute_white_24dp.svg";
+    }
+
+}
+
+
+
+function mudo() {
+
+    if (player.muted == true) {
+
+        //liga o som
+        player.muted = false;
+        console.log("Som Ativado");
+
+        document.getElementById('iconVol').src = "./assets/icons-player/volume_up_white_24dp.svg";
+
+        document.getElementById("volume").value = 100;
+        
+    }
+    else if (player.muted == false) {
+
+        //mudo
+        player.muted = true;
+        console.log("Ficou Mudo");
+
+        document.getElementById('iconVol').src = "./assets/icons-player/volume_off_white_24dp.svg";
+
+        document.getElementById("volume").value = 0;
+        
+    }
 }
